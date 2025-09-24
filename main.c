6c6245b202e493cf950391f0d11538974222acbf
taguhi-manukyan <taguhi.manukyan2004@gmail.com>
2025-09-24 16:24:28 +0000

    Initial commit for os_homework4

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main() {
+    int num = 5;
+
+    int res = square(num);
+
+    printf("The square of %d is %d\n", num, res);
+
+    return 0;
+}
+
---
+++ math_utils.h
@@ -0,0 +1,7 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+int square(int x);
+
+#endif
+
